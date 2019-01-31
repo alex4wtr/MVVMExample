a7127c5b862be9762ad3dfbefffcd6f422c8a7f4
alex4wtr <a.froehlich@wtr-architekten.de>
2019-01-31 21:36:52 +0100

    Initial Commit

---
+++ Model/StudentModel.cs
@@ -0,0 +1,59 @@
+﻿
+
+using System;
+using System.ComponentModel;
+
+namespace MVVMExample.Model
+{
+    public class StudentModel { }
+    public class Student : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    RaisePropertyChanged("FirstName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    RaisePropertyChanged("LastName");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get { return _firstName + " " + _lastName; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private void RaisePropertyChanged(string property)
+        {
+            // PropertyChanged?.Invoke checks the property for not beeing null
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+
+}
---
+++ ViewModel/StudentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MVVMExample.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMExample.ViewModel
+{
+    public class StudentViewModel
+    {
+        public StudentViewModel()
+        {
+            LoadStudents();
+        }
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>
+            {
+                new Student { FirstName = "Mark", LastName = "Allain" },
+                new Student { FirstName = "Allen", LastName = "Brown" },
+                new Student { FirstName = "Linda", LastName = "Hamerski" }
+            };
+
+            Students = students;
+        }
+    }
+}
